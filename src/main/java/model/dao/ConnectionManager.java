73c8dd438e82a0d76ee9356dc50a257e78a45ab3
KazuhiroYoshii <kazuhiro.yoshii@comsize.com>
2024-03-05 14:57:33 +0900

    よろしくお願いいたします。

---
+++ src/main/java/model/dao/ConnectionManager.java
@@ -0,0 +1,24 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+	
+	private final static String URL = "jdbc:mysql://localhost:3306/itemdb";
+	
+	private final static String USER = "root";
+	
+	private final static String PASSWORD = "root";
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		
+		//JDBCドライバの読み込み
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+		
+	}
+
+}
