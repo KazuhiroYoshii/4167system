8de3e97367346e29578594618ab77b3182251e7b
KazuhiroYoshii <kazuhiro.yoshii@comsize.com>
2024-03-06 16:23:37 +0900

    削除確認画面まで

---
+++ src/main/java/servlet/TaskDeleteServlet.java
@@ -0,0 +1,64 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.dao.TaskListDAO;
+import model.entity.UserCategoryStatusTaskBean;
+
+/**
+ * Servlet implementation class TaskDeleteServlet
+ */
+@WebServlet("/TaskDeleteServlet")
+public class TaskDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TaskDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// リクエストオブジェクトのエンコーディング方式の指定
+		request.setCharacterEncoding("UTF-8");
+		
+		//一覧表示画面で選択されたレコードのtaskIdを取得
+		int taskId = Integer.parseInt(request.getParameter("taskId"));
+		
+		//DAOをインスタンス化、当該レコードの詳細情報取得
+		TaskListDAO dao = new TaskListDAO();
+		UserCategoryStatusTaskBean task = dao.getTaskData(taskId);
+		
+		//セッションスコープに詳細情報を設定
+		HttpSession session = request.getSession();
+		session.setAttribute("task", task);
+		
+		//転送用オブジェクトの取得、転送
+		RequestDispatcher rd = request.getRequestDispatcher("delete-confirm.jsp");
+		rd.forward(request, response);
+		
+	}
+
+}
---
+++ src/main/webapp/delete-confirm.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" import="model.entity.UserCategoryStatusTaskBean" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>削除確認画面</title>
+</head>
+<body>
+	<%
+	//セッションスコープから選択したタスクの詳細情報を取得
+	UserCategoryStatusTaskBean task = (UserCategoryStatusTaskBean)session.getAttribute("task");
+	%>
+
+	<h1>削除確認画面</h1>
+	<h3>こちらのタスクを削除してもよろしいですか？</h3>
+	<table border = "1">
+		<tr><th>タスク名</th><td><%=task.getTaskName() %></td></tr>
+		<tr><th>カテゴリ情報</th><td><%=task.getCategoryName() %></td></tr>
+		<tr><th>期限</th><td><%=task.getLimitDate() %></td></tr>
+		<tr><th>担当者情報</th><td><%=task.getUserName() %></td></tr>
+		<tr><th>ステータス情報</th><td><%=task.getStatusName() %></td></tr>
+		<tr><th>メモ</th><td><%=task.getMemo() %></td></tr>
+	</table>
+</body>
+</html>
