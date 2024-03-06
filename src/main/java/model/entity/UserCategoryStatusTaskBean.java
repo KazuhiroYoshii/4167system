a90edf0779b31be319b6653f868b69d6447d179a
AikoSakurai0816 <aiko.sakurai@comsize.com>
2024-03-06 10:40:56 +0900

    UserCategoryStatusTaskBean

---
+++ src/main/java/model/entity/UserCategoryStatusTaskBean.java
@@ -0,0 +1,180 @@
+package model.entity;
+
+/**
+ * タスク登録・一覧・編集画面を作成するための情報を保持するクラス
+ * @author 櫻井
+ */
+public class UserCategoryStatusTaskBean {	
+	
+	// タスクID
+	private int taskId;
+	
+	// タスク名
+	private String taskName;
+	
+	// カテゴリID
+	private int categoryID;
+	
+	// カテゴリ名
+	private String categoryName;
+	
+	// 期限
+	private String limitDate;
+	
+	// ユーザID
+	private String userID;
+	
+	// ユーザ名
+	private String userName;
+	
+	// ステータスコード
+	private String statusCode;
+	
+	// ステータス名
+	private String statusName;
+	
+	// メモ
+	private String memo;
+
+	
+	/**
+	 * @return taskId
+	 */
+	public int getTaskId() {
+		return taskId;
+	}
+
+	/**
+	 * @return taskName
+	 */
+	public String getTaskName() {
+		return taskName;
+	}
+
+	/**
+	 * @return categoryID
+	 */
+	public int getCategoryID() {
+		return categoryID;
+	}
+
+	/**
+	 * @return categoryName
+	 */
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	/**
+	 * @return limitDate
+	 */
+	public String getLimitDate() {
+		return limitDate;
+	}
+
+	/**
+	 * @return userID
+	 */
+	public String getUserID() {
+		return userID;
+	}
+
+	/**
+	 * @return userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @return statusCode
+	 */
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	/**。
+	 * @return statusName
+	 */
+	public String getStatusName() {
+		return statusName;
+	}
+
+	/**
+	 * @return memo
+	 */
+	public String getMemo() {
+		return memo;
+	}
+
+	/**
+	 * @param taskId セットする taskId
+	 */
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	/**
+	 * @param taskName セットする taskName
+	 */
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	/**
+	 * @param categoryID セットする categoryID
+	 */
+	public void setCategoryID(int categoryID) {
+		this.categoryID = categoryID;
+	}
+
+	/**
+	 * @param categoryName セットする categoryName
+	 */
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	/**
+	 * @param limitDate セットする limitDate
+	 */
+	public void setLimitDate(String limitDate) {
+		this.limitDate = limitDate;
+	}
+
+	/**
+	 * @param userID セットする userID
+	 */
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	/**
+	 * @param userName セットする userName
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @param statusCode セットする statusCode
+	 */
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	/**
+	 * @param statusName セットする statusName
+	 */
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+
+	/**
+	 * @param memo セットする memo
+	 */
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	
+}
