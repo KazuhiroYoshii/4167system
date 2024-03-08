0b245a5c8150f712c39da098bc6046835acdc04e
AikoSakurai0816 <aiko.sakurai@comsize.com>
2024-03-08 16:02:44 +0900

    TaskAlterFormServlet（不正アクセス時にログイン画面へ遷移する機能の追加）

--- src/main/java/servlet/TaskAlterFormServlet.java
+++ src/main/java/servlet/TaskAlterFormServlet.java
@@ -36,8 +36,9 @@ public class TaskAlterFormServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
