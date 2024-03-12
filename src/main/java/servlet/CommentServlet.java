e6af41b627a703f2bf606f71953b5ee173b306fb
AikoSakurai0816 <aiko.sakurai@comsize.com>
2024-03-12 14:24:28 +0900

    コメント画面（CommentServlet）

---
+++ src/main/java/servlet/CommentServlet.java
@@ -0,0 +1,118 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.dao.CommentDAO;
+import model.entity.UserCategoryStatusTaskBean;
+import model.entity.UserCommentBean;
+
+/**
+ * コメント表示画面への遷移を制御する
+ * @author 櫻井
+ */
+@WebServlet("/CommentServlet")
+public class CommentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CommentServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * タスク一覧画面からの遷移を制御する
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// リクエストのエンコーディング方式を指定
+		request.setCharacterEncoding("UTF-8");
+
+		// リクエストパラメータの取得
+		int taskId = Integer.parseInt(request.getParameter("taskId"));
+
+		// DAOの生成
+		CommentDAO commentDao = new CommentDAO();
+
+		UserCategoryStatusTaskBean taskDetail = null;
+		List<UserCommentBean> commentList  = new ArrayList();
+
+		try {
+			// DAOを利用して該当タスクの詳細情報を取得
+			taskDetail = commentDao.selectTask(taskId);
+			// DAOを利用して該当タスクのコメント情報を取得
+			commentList = commentDao.selectComment(taskId);
+
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// タスク詳細情報をリクエストスコープに設定
+		request.setAttribute("taskDetail", taskDetail);
+		// コメント一覧表示用リストをリクエストスコープに設定
+		request.setAttribute("commentList", commentList);
+
+		// 転送用オブジェクトの取得、転送
+		RequestDispatcher rd = request.getRequestDispatcher("comment.jsp");
+		rd.forward(request, response);
+
+	}
+
+	/**
+	 * コメント投稿をしたときの遷移を制御する
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// リクエストのエンコーディング方式を指定
+		request.setCharacterEncoding("UTF-8");
+
+		// リクエストパラメータの取得
+		int taskId = Integer.parseInt(request.getParameter("taskId"));
+		String userId = request.getParameter("userId");
+		String comment = request.getParameter("comment");
+
+		// DAOの生成
+		CommentDAO commentDao = new CommentDAO();
+
+		int processingNumber = 0; // 処理件数
+		UserCategoryStatusTaskBean taskDetail = null;
+		List<UserCommentBean> commentList  = new ArrayList();
+
+		try {
+			// DAOを利用してコメントを追加
+			processingNumber = commentDao.insertComment(taskId);
+			
+			// DAOを利用して該当タスクの詳細情報を取得
+			taskDetail = commentDao.selectTask(taskId);
+			// DAOを利用して該当タスクのコメント情報を取得
+			commentList = commentDao.selectComment(taskId);
+
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// タスク詳細情報をリクエストスコープに設定
+		request.setAttribute("taskDetail", taskDetail);
+		// コメント一覧表示用リストをリクエストスコープに設定
+		request.setAttribute("commentList", commentList);
+		// タスク詳細情報をリクエストスコープに設定
+		request.setAttribute("taskDetail", taskDetail);
+
+		// 転送用オブジェクトの取得、転送
+		RequestDispatcher rd = request.getRequestDispatcher("comment.jsp");
+		rd.forward(request, response);
+	}
+
+}
