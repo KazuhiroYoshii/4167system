3295a30aba7e2ea6ddc4f57b10ea5b0f269602ef
KazuhiroYoshii <kazuhiro.yoshii@comsize.com>
2024-03-13 18:02:33 +0900

    引用元追記

--- src/main/webapp/js/deleteComment.js
+++ src/main/webapp/js/deleteComment.js
@@ -1,5 +1,8 @@
 /**
  * 削除実行前に確認ダイアログを表示する
+ * 
+ * 【引用元】Alterbo Inc. JavaScriptでダイアログボックスを表示するには
+ * https://alterbo.jp/blog/riri-210405/
  */
 function checkDelete(){
      var result = confirm('削除しますか？');
